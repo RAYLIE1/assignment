2e9c802451166b139675a590981e5ab35ed6ac7b
rayli <rayli@LAPTOP-K395GLJO>
2023-03-02 18:39:34 +0800

    Add project files.

---
+++ assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("Animal speaks");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Dog barks");
+    }
+
+    public void Speak(int volume)
+    {
+        Console.WriteLine($"Dog barks at volume {volume}");
+    }
+
+    public void Speak(string message)
+    {
+        Console.WriteLine($"Dog says: {message}");
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Animal animal = new Animal();
+        animal.Speak();
+
+        Dog dog = new Dog();
+        dog.Speak(); // method overriding
+
+        dog.Speak(5); // method overloading
+        dog.Speak("Hello"); // method overloading
+    }
+}
